58f107923e277395ec394d2f47348ff79e193778
maaref omar <64233476+omarmaaref@users.noreply.github.com>
2020-11-17 22:41:00 +0100

    Add files via upload

--- compte_rendu_java/src/compte_rendu_java/Priviligie.java
+++ compte_rendu_java/src/compte_rendu_java/Priviligie.java
@@ -2,6 +2,11 @@ package compte_rendu_java;
 
 public class Priviligie extends Client {
 private String societe ;
+public Priviligie (String societe) {
+	super();
+	this.societe=societe;
+	
+}
 public Priviligie (String[] arg,String societe) {
 	super(arg);
 	this.societe=societe;
--- compte_rendu_java/src/compte_rendu_java/Tourise.java
+++ compte_rendu_java/src/compte_rendu_java/Tourise.java
@@ -3,6 +3,10 @@ package compte_rendu_java;
 public class Tourise extends Client{
 private String  pays;
 //"client1","client1","C1",8,"France
+public Tourise (String pays) {
+	super();
+	this.pays=pays;
+}
 public Tourise (String[] arg,String pays) {
 	super(arg);
 	this.pays=pays;
